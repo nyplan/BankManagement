edf7d7e082a912140ad0f6079257825487a69678
teymur2234 <memmedliteymur060@gmail.com>
2023-05-07 13:43:04 +0400

    Add Bank Class and Create Structure for Data Seed and ad 3 new bank to database

---
+++ DataAccess/BankManagement.DAL/Contexts/DataSeed/BankDataSeed.cs
@@ -0,0 +1,41 @@
+﻿using BankManagement.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement.DAL.Contexts.DataSeed
+{
+    //this class use for data seed for bank table 
+    //every data seed must be follow this rule 
+    public static class BankDataSeed
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            builder.Entity<Bank>().HasData(
+             new Bank()
+            {
+                 Id = 1,
+                 Name= "Kapital Bank ",
+                 CreatedAt = DateTime.Now //this code will add added time to CreatedAt column
+            },
+             new Bank()
+             {
+                 Id= 2,
+                 Name= "ABB",
+                 CreatedAt = DateTime.Now
+             },
+             new Bank()
+             {
+                 Id= 3,
+                 Name="Merkezi Bank",
+                 CreatedAt= DateTime.Now
+             }
+             );
+        }
+    }
+}
---
+++ DataAccess/BankManagement.DAL/Entities/Bank.cs
@@ -0,0 +1,14 @@
+﻿using BankManagement.DAL.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement.DAL.Entities
+{
+    public class Bank:BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
