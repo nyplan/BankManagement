8abfa3731df5eac2dfac6a013f05adcaa289e7e8
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-05-07 19:21:37 +0400

    User added

---
+++ DataAccess/BankManagement.DAL/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using BankManagement.DAL.Entities.Common;
+
+namespace BankManagement.DAL.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Pin { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
